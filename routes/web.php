5f274811369f440604008096bd06a73e82d74a67
Giorgi Gavasheli <giorgi.gavasheli@shopboss.net>
2025-11-02 00:48:29 +0400

    Web.php fix

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Main/Index');
+    return Inertia::render('Index');
 });
 
 Route::get('/contact', function () {
