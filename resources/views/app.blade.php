e2a08cb2bf3debbfd3dc6c5724d47092c80e9edb
Giorgi Gavasheli <giorgi.gavasheli@shopboss.net>
2026-01-26 17:15:57 +0400

    ```md
    Upload built files to repository
    
    - Added compiled/generated build files to version control
    - Ensured latest build output is tracked alongside source code
    ```

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>George Gavasheli</title>
     <script src="https://kit.fontawesome.com/dc825ed24d.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes">
     @routes
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @inertiaHead
